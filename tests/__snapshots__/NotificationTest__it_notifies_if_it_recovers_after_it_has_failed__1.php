3b83df41af02642cf9cfb10e1574282910953537
pascalbaljet <pascal@pascalbaljet.nl>
2019-09-03 22:18:22 +0200

    Updated deps

--- tests/__snapshots__/NotificationTest__it_notifies_if_it_recovers_after_it_has_failed__1.php
+++
@@ -1,5 +0,0 @@
-<?php return [
-    0 => 'One of your checkers has recovered: Pbmedia\\ApiHealth\\Tests\\TestCheckers\\FailingAtOddTimesChecker.',
-    1 => 'It was in a failed state since 2018-07-01 14:00:00.',
-    2 => 'The previous exception message at the time it failed: TestChecker fails!',
-];
